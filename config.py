e5f4279c1159f0082ca47be7d969e5cdd00e0287
Chokeremy <pelageg@163.com>
2022-05-09 11:51:55 +0800

    update url

--- config.py
+++ config.py
@@ -3,7 +3,8 @@
 N = '*' * 50
 
 # trigger数据流接口url
-URL = 'http://172.16.1.2:38235/api/trigger/Trigger?dataSource=ETL'
+URL = 'http://172.16.1.2:38235/api/trigger/Trigger'
+# URL = 'http://synyi-cdss-trigger-720-test.sy/api/trigger/Trigger'
 
 # 数据库连接信息
 DATABASE_NAME = 'hdr_bank'
@@ -11,10 +12,15 @@ USER = 'postgres'
 PASSWORD = '765@#sy666'
 HOST = '172.16.1.2'
 PORT = 5432
+# DATABASE_NAME = 'hdr_cdss_test'
+# USER = 'gongqiaoshu'
+# PASSWORD = '123456'
+# HOST = '172.16.0.20'
+# PORT = 5432
 
 # 数据库查询符合条件的患者patient_id，visit_id触发trigger数据流接口
 # VTE   62 - 骨科  144 - 泌尿外科  266 - 产科
 # SQL = 'select patient_id, visit_id, source_visit_id from visit.visit_record where dept_id = 144 and out_time isnull limit 1 ;'
-SQL = 'select patient_id, visit_id, source_visit_id from visit.visit_record where dept_id = 62 and out_time isnull;'
-# SQL = 'select patient_id, visit_id, source_visit_id from visit.visit_record where dept_id = 62 and out_time isnull limit 1000; '
+# SQL = 'select patient_id, visit_id, source_visit_id from visit.visit_record where dept_id = 62 and out_time isnull;'
+SQL = 'select patient_id, visit_id, source_visit_id from visit.visit_record where dept_id = 62 and out_time isnull limit 1000; '
 # SQL = 'select patient_id, inpat_id, source_inpat_id from visit.inpat_record where current_dept_id = 62 and out_time isnull limit 1000;'
--- trigger.py
+++ trigger.py
@@ -15,7 +15,10 @@ class TriggerRequest:
         # 遍历所有数据，提取(patient_id, visit_id)数据组装发送请求打印返回结果
         for patient in self.json:
             try:
-                params = [
+                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                                         'like Gecko) Chrome/101.0.4951.41 Safari/537.36'}
+                params = {'dataSource': 'ETL'}
+                data = [
                     {
                         "patientId": patient[0],
                         "visitId": patient[1],
@@ -24,7 +27,7 @@ class TriggerRequest:
                 ]
 
                 # 发送post请求
-                self.response = requests.post(url=URL, json=params)
+                self.response = requests.post(url=URL, params=params, json=data, headers=headers)
 
                 # 打印返回结果
                 print('Trigger接口返回结果：{}\tVisitId：{}\tSourceVisitId：{}\t请求时间：{}'.
